5cb5a1ec6a32c802ce1da5479ef0bb28430adffa
WondwosenTsige <wondneh@gmail.com>
2021-09-22 21:02:08 -0700

    completed

--- python/code_challenges/graph/graph.py
+++ python/code_challenges/graph/graph.py
@@ -1,17 +1,15 @@
 class Graph:
     def __init__(self):
         self._adjacency_list = {}
-
-    def add_node(self, value):
+    def add_node(self, node):
         """
         add node
         Arguments: value
         Returns: The added node
         Add a node to the graph
         """
-        v = Vertex(value)
-        self._adjacency_list[v] = []
-        return v
+        self._adjacency_list[node] = []
+        return node
 
     def add_edge(self, start_vertex, end_vertex, weight=0):
         """
@@ -26,36 +24,35 @@ class Graph:
             raise KeyError("Starting Vertex not in Graph")
         if end_vertex not in self._adjacency_list:
             raise KeyError("End Vertex not in Graph")
-
         edge = Edge(end_vertex, weight)
         adjacencies = self._adjacency_list[start_vertex]
         adjacencies.append(edge)
-
     def get_nodes(self):
         """
         get nodes
         Arguments: none
         Returns all of the nodes in the graph as a collection (set, list, or similar)
         """
-
-    def get_neighbors(self):
+        return self._adjacency_list.keys()
+    def get_neighbors(self, vertex):
         """
         get neighbors
         Arguments: node
         Returns a collection of edges connected to the given node
-            Include the weight of the connection in the returned collection
+        Include the weight of the connection in the returned collection
         """
-
+        return self._adjacency_list[vertex]
     def size(self):
         """
         size
         Arguments: none
         Returns the total number of nodes in the graph
         """
+        return len(self._adjacency_list)
 
 
 class Vertex:
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.value = value
 
 
--- python/tests/test_graph.py
+++ python/tests/test_graph.py
@@ -1,6 +1,6 @@
 import pytest
 
-from graph.graph import Graph, Vertex, Edge
+from code_challenges.graph.graph import Graph, Vertex, Edge
 
 
 def test_can_instantiate_Graph():
@@ -16,7 +16,7 @@ def test_can_instantiate_Vertex():
 
 
 def test_can_instantiate_Edge():
-    vertex1 = Vertex("hello")
+    vertex1 = Vertex("selam")
     edge = Edge(vertex1, weight=50)
     assert edge.vertex == vertex1
     assert edge.weight == 50
@@ -24,7 +24,7 @@ def test_can_instantiate_Edge():
 
 def test_vertex_can_be_added_to_graph():
     graph = Graph()
-    vertex1 = Vertex("hello")
+    vertex1 = Vertex("selam")
     graph.add_node(vertex1)
     result = graph.size()
     assert result == 1
@@ -70,9 +70,11 @@ def test_neighbors_are_returned_with_weights_between_nodes_included():
     graph.add_edge(vertex1, vertex2, 20)
     graph.add_edge(vertex1, vertex3, 30)
     actual = graph.get_neighbors(vertex1)
+
     # actual is a list of edges
     # edges have vertex and weight properties
     # we want to check correct vertex and weight
+
     edge1 = actual[0]
     edge2 = actual[1]
     assert edge1.vertex == vertex2
@@ -92,10 +94,3 @@ def test_size_returns_correct_size():
     actual = graph.size()
     assert actual == 3
 
-
-# def test_a_graph_with_only_one_node_and_edge_can_be_returned():
-#     pass
-
-
-# def test_an_empty_graph_properly_returns_null():
-#     pass
